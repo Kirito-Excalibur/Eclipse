699a5d9dcd1c3c4377279a732c22df2b05d5fb85
Simon <nikhilsimontoppo@gmail.com>
2023-12-20 11:18:23 +0530

    Terminal Implemented

--- main.js
+++ main.js
@@ -1,12 +1,80 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+const fs = require("fs");
+const os = require("os");
+const pty = require("node-pty");
+const shell =
+  process.env.SHELL || (os.platform() === "win32" ? "powershell.exe" : "bash");
+const ptyProcess = pty.spawn(shell, ["-i"], {
+  name: "xterm-color",
+  cols: 90,
+  rows: 36,
+  cwd: process.env.HOME,
+  env: process.env,
+});
 
-    win.loadFile('index.html')
+//Context Menu skipped for now
+
+function readConf() {
+  const data = fs.readFileSync(__dirname + "/package.json", "utf8");
+  return data;
 }
 
-app.whenReady().then(createWindow)
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  win.loadFile("index.html");
+
+  //Event Listeners
+
+  ptyProcess.onData((data) => {
+    win.webContents.send("terminal.fromPty", data);
+  });
+
+  ptyProcess.onExit(() => {
+    win.close();
+  });
+
+  ipcMain.on("terminal.toPty", (event, data) => {
+    ptyProcess.write(data);
+  });
+
+  ipcMain.on("update-title", (event, title) => {
+    win.setTitle(title);
+  });
+
+  ipcMain.on("synchronous-message", (event, arg) => {
+    event.returnValue = readConf();
+  });
+
+  ipcMain.on("terminal.resize", (event, size) => {
+    if (ptyProcess && size.cols > 0 && size.rows > 0) {
+      ptyProcess.resize(size.cols, size.rows);
+    }
+  });
+
+  win.webContents.on("did-finish-load", () => {
+    ptyProcess.write("clear\r");
+  });
+
+  win.on("close", () => {
+    mainWindow = null;
+    app.quit();
+  });
+
+  app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+};
+
+app.whenReady().then(createWindow);
---
+++ renderer.js
@@ -0,0 +1,92 @@
+//Begin of Initialization
+
+window.$ = window.jQuery = require("jquery");
+const { app, BrowserWindow, ipcRenderer, shell, remote } = require("electron");
+const os = require("os");
+config = JSON.parse(ipcRenderer.sendSync("synchronous-message", ''));
+
+const { Terminal } = require("xterm");
+const terminal = new Terminal({
+  cursorBlink: true,
+});
+
+const { FitAddon } = require("xterm-addon-fit");
+const fitAddon = new FitAddon();
+terminal.loadAddon(fitAddon);
+
+const { WebLinksAddon } = require("xterm-addon-web-links");
+const handleLink = (event, uri) => {
+  event.preventDefault();
+  shell.openExternal(uri);
+};
+const webLinksAddon = new WebLinksAddon(handleLink);
+terminal.loadAddon(webLinksAddon);
+
+// End of Initialization
+
+// Begin of Functions
+
+function resize() {
+  fitAddon.fit();
+}
+
+function send(text) {
+  text = text.trim();
+  ipcRenderer.send("terminal.toPty", text);
+}
+
+//End of Functions
+
+jQuery(document).ready(function () {
+  console.log("App Name: " + config.name);
+  console.log("App Title: " + config.title);
+  console.log("App Version: " + config.version);
+  console.log("Node.js Version: " + process.version);
+  console.log("Chromium Version: " + process.versions["chrome"]);
+  console.log("Electron Version: " + process.versions.electron);
+  console.log("jQuery Version: " + jQuery.fn.jquery);
+  jQuery(".title").html(config.title);
+
+  //Begin of Run On Ready
+
+  terminal.open(document.getElementById("terminal"));
+
+  jQuery("#terminal textarea").focus()
+
+  resize();
+
+  //End of Run On Ready
+
+  //Begin of Event Listeners
+
+  terminal.onData((data) => {
+    ipcRenderer.send("terminal.toPty", data);
+  });
+
+  ipcRenderer.on("terminal.fromPty", (event, data) => {
+    terminal.write(data);
+  });
+
+  terminal.onTitleChange((title) => {
+    ipcRenderer.send("update-title", title);
+  });
+
+  terminal.attachCustomKeyEventHandler((arg) => {
+    if (arg.ctrlKey && arg.code === "KeyV" && arg.type === "keydown") {
+      arg.preventDefault();
+      navigator.clipboard.readText().then((text) => {
+        send(text);
+      });
+    }
+
+    return true;
+  });
+
+  window.addEventListener("resize", () => {
+    resize();
+  });
+
+  terminal.onResize((size) => {
+    ipcRenderer.send("terminal.resize", size);
+  });
+});
